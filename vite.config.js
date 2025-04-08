9269cf3eb8ea9f8e1022366b7b4c1a2f54e328d2
meixiyao <1397912694@qq.com>
2025-04-08 18:20:54 +0800

    部署

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
         { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
       ],
     },
-    base: './',
+    base: mode === 'development' ? './' : '/viewer/',
 })
